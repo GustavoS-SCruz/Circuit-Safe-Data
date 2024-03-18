3cf2f8d8a02c3a314322ddc7b58c43b1fe7dfc38
Angelly Rondon <angelly.tonani@sptech.school>
2024-03-18 12:49:48 -0300

    Adicionando funcionaliade de login

---
+++ system-login/be-circuit-safe/src/main/java/Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+
+    String nomeDeUsuario;
+    String senha;
+
+    public Usuario(String nomeDeUsuario, String senha) {
+        this.nomeDeUsuario = nomeDeUsuario;
+        this.senha = senha;
+    }
+}
